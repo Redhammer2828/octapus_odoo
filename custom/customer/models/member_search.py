73c0843cfb5b3a395d46496ee922f5dfa94ce061
Jishad <120619326+JishadGit@users.noreply.github.com>
2024-04-09 15:27:37 +0530

    changes

--- custom/customer/models/member_search.py
+++ custom/customer/models/member_search.py
@@ -11,12 +11,12 @@ class MemberSearch(models.Model):
     email = fields.Char(string="Email")
     old_membership_number = fields.Char(string="Old Membership Number")
     vehicle_plate = fields.Char(string="Plate Number")
-    member_name = fields.Char(string="Member Name")
+    name = fields.Char(string="Name")
     customer_id = fields.Many2one('res.partner', string="Customer", domain="[('is_company', '=', True), ('customer', '=', True)]")
 
     def search_member(self):
         partner_obj = self.env['res.partner']
-        search_domain = []
+        search_domain = [('member_type', '=', 'policy')]  # Filter by member_type = policy
 
         # Build the search domain based on the user-entered criteria
         if self.policy_no:
@@ -33,13 +33,13 @@ class MemberSearch(models.Model):
             search_domain.append(('vehicle_chasis_no', 'ilike', self.vehicle_chasis_no))
         if self.vehicle_plate:
             search_domain.append(('vehicle_plate', 'ilike', self.vehicle_plate))
-        if self.member_name:
-            search_domain.append(('name', 'ilike', self.member_name))
+        if self.name:
+            search_domain.append(('name', 'ilike', self.name))
         if self.customer_id:
             search_domain.append(('id', '=', self.customer_id.id))
 
         # Search for partners based on the search domain
-        partner_ids = partner_obj.search(search_domain)
+        partner_ids = partner_obj.search(search_domain, order='create_date desc')
 
         # Open the tree view with the search results
         return {
@@ -48,9 +48,51 @@ class MemberSearch(models.Model):
             'res_model': 'res.partner',
             'type': 'ir.actions.act_window',
             'domain': [('id', 'in', partner_ids.ids)],
-            'views': [(self.env.ref('customer.policy_member_search_view_tree').id, 'tree')],
+            'views': [
+                (self.env.ref('customer.res_partner_member_tree').id, 'tree'),
+                (self.env.ref('customer.res_partner_member_form').id, 'form'),
+                ]
+
         }
-    
+
+        
     def search_credit_member(self):
         partner_obj = self.env['res.partner']
-        search_domain = []
+        search_domain = [('member_type', '=', 'credit')]  # Filter by member_type = policy
+
+        # Build the search domain based on the user-entered criteria
+        if self.policy_no:
+            search_domain.append(('policy_no', 'ilike', self.policy_no))
+        if self.ref_num:
+            search_domain.append(('ref_num', 'ilike', self.ref_num))
+        if self.mobile:
+            search_domain.append(('mobile', 'ilike', self.mobile))
+        if self.email:
+            search_domain.append(('email', 'ilike', self.email))
+        # if self.old_membership_number:
+        #     search_domain.append(('old_membership_number', 'ilike', self.old_membership_number))
+        if self.vehicle_chasis_no:
+            search_domain.append(('vehicle_chasis_no', 'ilike', self.vehicle_chasis_no))
+        if self.vehicle_plate:
+            search_domain.append(('vehicle_plate', 'ilike', self.vehicle_plate))
+        if self.name:
+            search_domain.append(('name', 'ilike', self.name))
+        if self.customer_id:
+            search_domain.append(('id', '=', self.customer_id.id))
+
+        # Search for partners based on the search domain
+        partner_ids = partner_obj.search(search_domain, order='create_date desc')
+
+        # Open the tree view with the search results
+        return {
+            'name': 'Policy Member Search Results',
+            'view_mode': 'tree,form',
+            'res_model': 'res.partner',
+            'type': 'ir.actions.act_window',
+            'domain': [('id', 'in', partner_ids.ids)],
+            'views': [
+                (self.env.ref('customer.res_partner_credit_member_tree').id, 'tree'),
+                (self.env.ref('customer.res_partner_credit_member_form').id, 'form'),
+                ]
+
+        }
