bd098ac8fc5f08e197f222cd089a85bf5bbc295d
Jishad <120619326+JishadGit@users.noreply.github.com>
2024-05-22 14:40:09 +0530

    changes

--- custom/customer/wizards/upload_member_wizard.py
+++ custom/customer/wizards/upload_member_wizard.py
@@ -55,6 +55,7 @@ class UploadMemberWizard(models.TransientModel):
                 'package': row.get('package_id'),
                 'customer_code': row.get('customer_code'),
                 'sequence_code': row.get('category_code'),
+                
                 # Add other fields from the Excel file as needed
             }
             member_lines.append((0, 0, member_line_data))
