d63179063ed0f65a4046179d7f794295b9725562
Jishad <120619326+JishadGit@users.noreply.github.com>
2024-10-04 10:51:35 +0530

    changes

--- custom/customer/models/location.py
+++ custom/customer/models/location.py
@@ -22,13 +22,13 @@ class LocationService(models.Model):
 
     amount = fields.Integer('Amount')
     from_h3index = fields.Char('From H3index')
-    from_latitude = fields.Float('From Latitude')
+    from_latitude = fields.Float('From Latitude',digits=(10, 8))
     from_location = fields.Char('From Location')
-    from_longitude = fields.Float('From Longitude')
+    from_longitude = fields.Float('From Longitude',digits=(10, 8))
     to_h3index = fields.Char('To H3index')
-    to_latitude = fields.Float('To Latitude')
+    to_latitude = fields.Float('To Latitude',digits=(10, 8))
     to_location = fields.Char('To Location')
-    to_longitude = fields.Float('To Longitude')
+    to_longitude = fields.Float('To Longitude',digits=(10, 8))
 
 class LocationFrom(models.Model):
     _name = 'location.latlong'
