5fab419c54d8739f6f6648705144fd82532cb7a9
Aparnnaraj <aparnnaraj232020@gmail.com>
2025-04-26 13:35:11 +0530

    RAC BULK REPORT

--- custom/customer/__manifest__.py
+++ custom/customer/__manifest__.py
@@ -74,6 +74,7 @@
         'wizards/policy_report_wizard_view.xml',
         'wizards/vendors_report_wizard.xml',
         'wizards/aaa_rac_wizard.xml',
+        'wizards/rac_bulk_upload.xml',
         'data/scheduled_action.xml',
         'data/sequence_data.xml',
         'data/server_action.xml',
