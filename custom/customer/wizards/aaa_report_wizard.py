bff25a01aaf3084884958961ea1cf44dc18f15fd
Aparnnaraj <aparnnaraj232020@gmail.com>
2025-05-13 13:37:29 +0530

    ADDED JAFZA DRIVER AND AFTER DUTY IN CALL CENTER REPORT

--- custom/customer/wizards/aaa_report_wizard.py
+++ custom/customer/wizards/aaa_report_wizard.py
@@ -63,7 +63,8 @@ class AaaReportWizard(models.TransientModel):
             'name', 'service_time', 'create_date', 'customer_id', 'sequence_id',
             'membership_num', 'member_id','member_type', 'member_contact_no', 'policy_no',
             'type', 'vehicle_type', 'vehicle_plate', 'vehicle_chasis_no', 'product_id', 'provider_id',
-            'driver_id', 'driver_name', 'driver_num', 'selected_from_location', 'from_location',
+            'driver_id', 'driver_name', 'driver_num','is_jafza_service', 'jafza_provider_id', 'jafza_driver_id', 
+            'jafza_driver_name', 'is_aditional_duty', 'selected_from_location', 'from_location',
             'from_location_emirate', 'selected_to_location', 'to_location',
             'to_location_emirate', 'date_time_from', 'date_time_to', 'smarto_id',
             'state', 'credit_proforma_number', 'cash_collected', 'vendor_rating',
@@ -223,7 +224,7 @@ class AaaReportWizard(models.TransientModel):
             'Number', 'Service Date & Time', 'Created Date & Time', 'Customer Name', 'Category',
             'Membership Number', 'Member Name', 'Membership State', 'Mobile', 'Policy Number',
             'Member Type', 'Service Type', 'Vehicle Type', 'Vehicle Plate', 'Vehicle Chasis No.', 'In Progress Date and Time',
-            'Service', 'Provider', 'Driver', 'Driver Mobile Number', 'From - Location',
+            'Service', 'Provider', 'Driver', 'Driver Mobile Number', 'Jafza Driver', 'After Duty', 'From - Location',
             'To - Location',
             'From Emirate', 'To Emirate'  # Always include these columns now
         ]
@@ -337,6 +338,10 @@ class AaaReportWizard(models.TransientModel):
                 record.get('driver_id')[1] if record.get('driver_id') else record.get('driver_name', ''),
 
                 record.get('driver_num', ''),
+                record.get('jafza_driver_id')[1] if record.get('jafza_driver_id') else record.get('jafza_driver_name', ''),
+                #record.get('jafza_driver_id', '') if record.get('jafza_provider_id') == "ARABIAN AUTOMOBILE ASSOCIATION" else record.get('jafza_driver_name', ''),
+                record.get('is_aditional_duty',''),
+
                 record.get('selected_from_location')[1] if (record.get('selected_from_location') and record.get('member_type') in ['policy', 'adhoc']) else (
                     record.get('from_location')[1] if record.get('from_location') else ''
                 ),
