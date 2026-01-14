ec08d7a3feecb56847b5bca274752682d6b7d162
Sreesanth <sreesanth@radiantwtech.in>
2026-01-14 14:45:28 +0530

    Schedule service - package in membership check

--- custom/customer/wizards/service_dispatch_wizard.py
+++ custom/customer/wizards/service_dispatch_wizard.py
@@ -6,11 +6,26 @@ class ServiceDispatchWizard(models.TransientModel):
     _description = 'Service Dispatch Wizard'
 
     service_id = fields.Many2one('aaa.service', string="Service")
+    is_schedule_service = fields.Boolean(string="Is Schedule Service", default=False)
     # message = fields.Text(string="Message", default="Service not in package, proceed with cash/credit service?")
 
     def action_cash_service(self):
         # Set cash type then route through dispatch method which performs validation
         self.service_id.write({'type': 'cash'})
+        
+        if self.is_schedule_service:
+            return {
+                'name': _('Schedule Service'),
+                'type': 'ir.actions.act_window',
+                'res_model': 'schedule.service.wizard',
+                'view_mode': 'form',
+                'view_id': self.env.ref('customer.schedule_service_wizard_view_form').id,
+                'target': 'new',
+                'context': {
+                    'default_service_id': self.service_id.id,
+                },
+            }
+            
         self.create_service_history()
         self.service_id._dispatch_service()
         return {'type': 'ir.actions.act_window_close'}
@@ -18,6 +33,20 @@ class ServiceDispatchWizard(models.TransientModel):
     def action_credit_service(self):
         # Set member_type to credit then route through dispatch for consistent validation
         self.service_id.write({'member_type': 'credit'})
+
+        if self.is_schedule_service:
+            return {
+                'name': _('Schedule Service'),
+                'type': 'ir.actions.act_window',
+                'res_model': 'schedule.service.wizard',
+                'view_mode': 'form',
+                'view_id': self.env.ref('customer.schedule_service_wizard_view_form').id,
+                'target': 'new',
+                'context': {
+                    'default_service_id': self.service_id.id,
+                },
+            }
+        
         self.create_service_history()
         self.service_id._dispatch_service()
         return {'type': 'ir.actions.act_window_close'}
